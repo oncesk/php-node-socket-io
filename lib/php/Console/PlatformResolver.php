c4f5e0bd0316739b924df63265cea78fe235ff93
Once <unnfly@gmail.com>
2014-03-14 23:26:09 +0300

    implement platform resolver interface in PlatformResolver

--- lib/php/Console/PlatformResolver.php
+++ lib/php/Console/PlatformResolver.php
@@ -6,13 +6,17 @@ namespace PhpNodeSocketIO\Console;
  * @package PhpNodeSocketIO\Console
  */
 class PlatformResolver implements PlatformResolverInterface {
-	
+
 	/**
 	 * Should return platform which defined in Platform class
 	 *
 	 * @return int const
 	 */
 	public function resolve() {
-		// TODO: Implement resolve() method.
+		if (strpos(PHP_OS, 'WIN') !== false) {
+			return Platform::WINDOWS;
+		} else {
+			return Platform::UNIX;
+		}
 	}
 }
