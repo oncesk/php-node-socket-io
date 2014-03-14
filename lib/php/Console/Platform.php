765c5dd05f79091305a072adc72043e65df4eaba
Once <unnfly@gmail.com>
2014-03-14 23:24:39 +0300

    create platform resolver

--- lib/php/Console/Platform.php
+++ lib/php/Console/Platform.php
@@ -9,6 +9,4 @@ class Platform {
 
 	const UNIX      = 1;
 	const WINDOWS   = 2;
-
-
 }
---
+++ lib/php/Console/PlatformResolver.php
@@ -0,0 +1,18 @@
+<?php
+namespace PhpNodeSocketIO\Console;
+
+/**
+ * Class PlatformResolver
+ * @package PhpNodeSocketIO\Console
+ */
+class PlatformResolver implements PlatformResolverInterface {
+	
+	/**
+	 * Should return platform which defined in Platform class
+	 *
+	 * @return int const
+	 */
+	public function resolve() {
+		// TODO: Implement resolve() method.
+	}
+}
